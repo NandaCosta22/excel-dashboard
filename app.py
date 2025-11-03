5dc1018d930893031b65c7ecfb0e9f2a2befdf97
Fernanda Araujo da Costa <nandaajobsk@outlook.com>
2025-11-03 22:42:33 +0000

    adiciona flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
 
 
-app = Flask(__name__)
+app = flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
